e44db8ff5226a15c7c7f98070a40c5abbe73f40e
michaelchu <mchchu88@gmail.com>
2018-06-09 16:42:44 -0400

    Implemented and test option symbol generator function

--- optopsy/__init__.py
+++ optopsy/__init__.py
@@ -1,18 +1,18 @@
 from . import filters as filters
+from . import globals as globals
+from . import helpers as helpers
 from . import option_query as option_query
 from . import options as options
 from . import optopsy as optopsy
 from . import strategy as strategy
-from . import globals as globals
-
-
 from .data import get as get
 from .data import gets as gets
 from .filters import *
+from .globals import *
+from .helpers import *
 from .option_query import *
 from .options import *
 from .optopsy import *
 from .strategy import *
-from .globals import *
 
 
--- optopsy/helpers.py
+++ optopsy/helpers.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import re
+
+import pandas as pd
 
 
 def generate_symbol(sym, exp, strike, opt_type):
@@ -11,10 +12,24 @@ def generate_symbol(sym, exp, strike, opt_type):
         - Option type, either P or C, for put or call
         - Strike price, as the price x 1000, front padded with 0s to 8 digits
     """
+
+    # remove any non alphabetic characters from symbol
+    regex = re.compile('[^a-zA-Z]')
+    sym = regex.sub('', sym).upper()
+
+    # format the expiration date with 6 digits, no space, two characters
     expiration = pd.to_datetime(exp).strftime("%y%m%d")
-    opt_type = opt_type.upper()
+
+    # format option type to be one char, capitalized
+    valid = ('c', 'p', 'call', 'put')
+    if opt_type.lower() not in valid:
+        raise ValueError("Invalid option type character!")
+    else:
+        opt_type = opt_type[:1].upper()
+
+    # format strike to be 8 digits padded, with strike price multiplied by 1000
     strike = "{0:0>8}".format(int(strike * 1000))
-    
+
     return "%s%s%s%s" % (sym, expiration, opt_type, strike)
 
 
