e32d960e402cedd7b0bf9bfba383c8f12b568bd3
michaelchu <mchchu88@gmail.com>
2018-06-08 21:42:23 -0400

    fixed Globals import issue

--- optopsy/__init__.py
+++ optopsy/__init__.py
@@ -3,6 +3,9 @@ from . import option_query as option_query
 from . import options as options
 from . import optopsy as optopsy
 from . import strategy as strategy
+from . import globals as globals
+
+
 from .data import get as get
 from .data import gets as gets
 from .filters import *
@@ -10,3 +13,6 @@ from .option_query import *
 from .options import *
 from .optopsy import *
 from .strategy import *
+from .globals import *
+
+
