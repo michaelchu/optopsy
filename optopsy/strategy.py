27a4761e605530d47a0a24ba9d1321a7984d12de
Michael Chu <mchchu88@gmail.com>
2018-06-17 23:05:28 -0400

    WIP - filters

--- optopsy/strategy.py
+++ optopsy/strategy.py
@@ -2,28 +2,23 @@ from .enums import FilterType
 from .filters import FilterStack
 import optopsy as op
 
+
 class Strategy(object):
 
-    def __init__(self, name, opt_strat, filters):
+    def __init__(self, name, option_strategy, filters):
 
-        if not isinstance(opt_strat, op.OptionStrategy):
-            raise ValueError("opt_strat parameter must be of OptionStrategy type!")
+        if not isinstance(option_strategy, op.OptionStrategy):
+            raise ValueError("option_strategy parameter must be of OptionStrategy type!")
 
         if not isinstance(filters, list):
             raise ValueError("filters must of list type")
 
         self.name = name
-
-        self.entry_filters = [f for f in filters if f.type == op.FilterType.ENTRY]
-        self.exit_filters = [f for f in filters if f.type == op.FilterType.EXIT]
-
-        self.opt_strat = opt_strat
+        self.option_strategy = option_strategy
 
         self.spread_data = None
-        self.latest_quote = None
-
-        self.entry_stack = FilterStack(self.entry_filters)
-        self.exit_stack = FilterStack(self.exit_filters)
+        self.filter_stack = None
+        self.filters = filters
 
         self.now = None
         self.bankrupt = False
@@ -31,21 +26,18 @@ class Strategy(object):
 
     def setup(self, data):
         # call the option object to construct the spread
-        self.spread_data = self.opt_strat(data)
+        self.spread_data = self.option_strategy(data)
+        self.filter_stack = FilterStack(self)
 
     def update(self, date):
         # current quote date
         self.now = date
 
         # we will slice our spread_data by today's date
-        self.latest_quote = op.OptionQuery(self.spread_data.loc[self.now])
+        latest_quote = op.OptionQuery(self.spread_data.loc[self.now])
 
         # run entry filters against our current quote prices
-        self.entry_stack(self)
-
-        # if we have positions, run exit filters against our current positions
-        if not self.positions:
-            self.exit_stack(self)
+        self.filter_stack(self, latest_quote)
 
     def adjust(self, amount):
         pass
