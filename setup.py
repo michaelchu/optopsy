1f44caec33a69b10e62a3434eb1252bc82d578fe
moue <yuqi.hou@gmail.com>
2018-08-03 07:31:10 -0400

    updated installation instructions and sample strategy (#3)

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(name='optopsy',
       description='Python Backtesting library for options trading strategies',
-      long_description='Python Backtesting library for options trading strategies',
-      version='1.0.0',
+      long_description=open("README.md").read(),
+      version='0.1.0',
       url='https://github.com/michaelchu/optopsy',
       author='Michael Chu',
       author_email='mchchu88@gmail.com',
