7a77f3a5d6be60c280439b7b2e17e5da8abce233
michaelchu <mchchu88@gmail.com>
2018-08-04 14:05:03 -0400

    refactor-fn init commit

--- optopsy/data.py
+++ optopsy/data.py
@@ -32,56 +32,57 @@ fields = (
 )
 
 
-def _import(path, start, end, struct, skiprows, prompt, bulk):
-    """
-    This method will read in the data file using pandas and assign the
-    normalized dataframe to the class's data variable.
+def _read_file(path, cols, sr, nrows=None):
+    return pd.read_csv(path, parse_dates=True, names=cols[0], usecols=cols[1], skiprows=sr,
+                       nrows=nrows)
 
-    Normalization means to map columns from data source to a standard
-    column name that will be used in this program.
 
-    :file_path: the path of the file relative to the current file
-    :start: date object representing the start date to include in the backtest
-    :end: date object representing the end date to include in the backtest
-    :struct: a list of dictionaries to describe the column index to read in the option chain data
-    """
+def _import_file(path, cols, start, end, struct, sr):
+    if _check_file_exists(path):
+        return _read_file(path, cols, sr).pipe(_format)
 
-    if not bulk and os.path.isdir(path):
+
+def _import_dir_files(path, cols, start, end, struct, sr):
+    if _check_file_path_exists(path):
+        fls = glob.glob(os.path.join(path, "*.csv")).sort()
+        return pd.concat(_read_file(f, cols, sr) for f in fls).pipe(_format)
+
+
+def _check_file_exists(path):
+    if os.path.isdir(path):
         raise ValueError("Invalid path, please provide a valid path to a file")
-    elif bulk and not os.path.isdir(path):
+    return True
+
+
+def _check_file_path_exists(path):
+    if not os.path.isdir(path):
         raise ValueError("Invalid path, please provide a valid directory path")
-    else:
-        cols = _check_structs(struct, start, end)
+    return True
 
+
+def _do_preview(path, cols, sr):
+    print(_read_file(path, cols, sr, nrows=5).pipe(_format))
+    return user_prompt("Does this look correct?")
+
+
+def get(file_path, start, end, struct, sr=1):
+    return _do_import(file_path, start, end, struct, sr)
+
+
+def gets(dir_path, start, end, struct, sr=1):
+    return _do_import(dir_path, start, end, struct, sr, bulk=True)
+
+
+def _do_import(path, start, end, struct, sr, bulk=False):
+    if _check_structs(struct):
+        cols = list(zip(*struct))
+    if _do_preview(path, cols, sr):
         if bulk:
-            # for each file in path
-            all_files = glob.glob(os.path.join(path, "*.csv"))
-            all_files.sort()
-            df = pd.concat(
-                pd.read_csv(f, parse_dates=True, names=cols[0], usecols=cols[1], skiprows=skiprows)
-                for f in all_files)
+            return _import_dir_files(path, cols, start, end, struct, sr)
         else:
-            df = pd.read_csv(path, parse_dates=True, names=cols[0], usecols=cols[1],
-                             skiprows=skiprows)
-
-        # TODO: We need to ensure that only one symbol of the underlying asset is returned
-
-        if prompt:
-            print(df.head())
-            if user_prompt("Does this look correct?"):
-                return _format(df).loc[start:end]
-        elif not prompt:
-            return _format(df).loc[start:end]
-        else:
-            sys.exit()
-
-
-def get(file_path, start, end, struct, skiprows=1, prompt=True):
-    return _import(file_path, start, end, struct, skiprows, prompt, bulk=False)
-
-
-def gets(dir_path, start, end, struct, skiprows=1, prompt=True):
-    return _import(dir_path, start, end, struct, skiprows, prompt, bulk=True)
+            return _import_file(path, cols, start, end, struct, sr)
+    else:
+        sys.exit()
 
 
 def _format(df):
@@ -90,35 +91,57 @@ def _format(df):
     :param df: dataframe to format
     :return: formatted dataframe
     """
-    df['expiration'] = pd.to_datetime(df['expiration'], infer_datetime_format=True,
-                                      format='%Y-%m-%d')
-    df['quote_date'] = pd.to_datetime(df['quote_date'], infer_datetime_format=True,
-                                      format='%Y-%m-%d')
 
-    # convert option types to standard format 'c' or 'p'
-    df['option_type'] = df['option_type'].str.lower().str[:1]
+    return (df
+        .assign(expiration=lambda r: pd.to_datetime(r['expiration'], infer_datetime_format=True, format='%Y-%m-%d'),
+                quote_date=lambda r: pd.to_datetime(r['quote_date'], infer_datetime_format=True, format='%Y-%m-%d'),
+                option_type=lambda r: r['option_type'].str.lower().str[:1])
+        .set_index('quote_date', drop=False)
+        .round(2)
+        .pipe(_assign_option_symbol)
+    )
 
-    # use quote date as index
-    df.set_index('quote_date', inplace=True, drop=False)
 
-    # rounds numbers to two decimals
-    df = df.round(2)
-
+def _assign_option_symbol(df):
     # if the data source did not include a option_symbol field, we will generate it
     if 'option_symbol' in df.columns:
-        df = df.drop('symbol', axis=1)
-        df = df.rename(columns={'option_symbol': 'symbol'})
-        df['symbol'] = '.' + df['symbol']
+        return (df
+                .drop('symbol', axis=1)
+                .rename(columns={'option_symbol': 'symbol'})
+                .assign(symbol=lambda r: '.' + r['symbol'])
+        )
     else:
+        # TODO: vertorize this method, avoid using df.apply()
         df['symbol'] = '.' + df.apply(
-            lambda r: generate_symbol(r['symbol'], r['expiration'], r['strike'], r['option_type']),
-            axis=1
+            lambda r: generate_symbol(r['symbol'], r['expiration'], r['strike'], r['option_type']), axis=1
         )
 
-    return df
+
+def _check_field_is_standard(struct):
+    # First we check if the provided struct uses our standard list of defined column names
+    std_fields = list(zip(*fields))[0]
+    for f in struct:
+        if f[0] not in std_fields or f[1] < 0:
+            raise ValueError("Field names or field indices not valid!")
+    return True
 
 
-def _check_structs(struct, start, end):
+def _check_field_is_duplicated(struct):
+    # check if we have any duplicate indices, which would be invalid
+    if len(list(set(cols[1]))) != len(cols[1]):
+        raise ValueError("Duplicate indices found!")
+    return True
+
+
+def _check_fields_contains_required(struct):
+    # Check if the struct provided contains all the required fields
+    req_fields = [x[0] for x in fields if x[1] is True]
+    if not all(f in cols[0] for f in req_fields):
+        raise ValueError("Required field names not defined!")
+    return True
+
+
+def _check_structs(struct):
     """
     This method will check the provided struct for this data set and make sure the
     provided fields and indices are valid
@@ -126,28 +149,11 @@ def _check_structs(struct, start, end):
     :param end: the end date of all imported data
     :param struct: a list containing tuples that contain the column name and the index referring to
     the column number to import from
-    :return:True or False
+    :return: a list of column names and it's index
     """
-
-    std_fields = list(zip(*fields))[0]
-    req_fields = [x[0] for x in fields if x[1] is True]
-
-    # First we check if the provided struct uses our standard list of defined column names
-    for f in struct:
-        if f[0] not in std_fields or f[1] < 0 or start > end:
-            raise ValueError("Field names or field indices not valid!")
-
-    cols = list(zip(*struct))
-
-    # check if we have any duplicate indices, which would be invalid
-    if len(list(set(cols[1]))) != len(cols[1]):
-        raise ValueError("Duplicate indices found!")
-
-    # Check if the struct provided contains all the required fields
-    if not all(f in cols[0] for f in req_fields):
-        raise ValueError("Required field names not defined!")
-
-    return cols
+    return (_check_field_is_standard(struct) and 
+            _check_field_is_duplicated(struct) and
+            _check_fields_contains_required(struct))
 
 
 def user_prompt(question):
