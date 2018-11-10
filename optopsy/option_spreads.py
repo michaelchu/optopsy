2acf374f422ac0c1bc36ee5aefa35b21100fade3
Michael Chu <mchchu88@gmail.com>
2018-11-10 10:39:42 -0500

    Implementation of filters basic leg filters (#7)

--- optopsy/option_spreads.py
+++ optopsy/option_spreads.py
@@ -1,16 +1,44 @@
-from .option_queries import nearest, eq, opt_type
+from .enums import Period
+from .option_queries import opt_type
+from functools import reduce
+import optopsy.filters as fil
 import pandas as pd
 
 
-def create_legs(data, legs):
-    def _create_leg(leg):
-        return (
-            data
-            .pipe(opt_type, option_type=leg[0])
-            .pipe(nearest, 'delta', leg[1])
-            .pipe(eq, 'dte', leg[2])
-            .assign(ratio=leg[3])
-        )
+default_entry_filters = {
+    "std_expr": False,
+    "contract_size": 10,
+    "entry_dte": Period.FOUR_WEEKS.value,
+}
 
-    spread = [_create_leg(legs[l]) for l in range(0, len(legs))]
-    return spread[0] if len(legs) == 1 else pd.concat(spread).reset_index(drop=True)
+
+def _create_legs(data, leg):
+    return (
+        data
+        .pipe(opt_type, option_type=leg[0])
+        .assign(ratio=leg[1])
+    )
+
+
+def _apply_filters(legs, filters):
+    if not filters:
+        return legs
+    else:
+        return [reduce(lambda l, f: getattr(fil, f)(l, filters[f], idx), filters, leg)
+                for idx, leg in enumerate(legs)]
+
+
+def create_spread(data, leg_structs, filters):
+    sort_by = ['quote_date', 'option_type', 'expiration', 'strike']
+    legs = [_create_legs(data, leg) for leg in leg_structs]
+
+    # merge and apply leg filters to create spread
+    entry_filters = {**default_entry_filters, **filters}
+    spread = pd.concat(_apply_filters(
+        legs, entry_filters)).sort_values(sort_by)
+
+    # apply spread level filters to spread
+    spread_filters = {f: filters[f]
+                      for f in filters if f.startswith('entry_spread')}
+    return pd.concat(_apply_filters(
+        [spread], spread_filters)).sort_values(sort_by)
