9d6d2e1bd2f68dfd53d97186d858f3bdf81b88df
Michael Chu <mchchu88@gmail.com>
2018-11-12 01:00:55 -0500

    Implemented Run method for backtest engine (#8)
    * Initial implementation of backtest engine
    
    * Added start and end dates for option strategies
    
    * Improved data import performance, updated test files to use modular fixtures
    
    * Renamed and moved backtest functions into backtest.py
    Initial implementation of 'run' method
    
    * Implemented Run method, added verticals integration test

--- optopsy/backtest.py
+++ optopsy/backtest.py
@@ -1,3 +1,104 @@
+from functools import reduce
+
+import pandas as pd
+
+import optopsy.filters as fil
+from .enums import Period
+from .option_queries import opt_type
+
+pd.set_option('display.expand_frame_repr', False)
+
+sort_by = [
+    'underlying_symbol',
+    'quote_date',
+    'option_type',
+    'expiration',
+    'strike']
+on = ['underlying_symbol', 'option_type', 'expiration', 'strike']
+
+default_entry_filters = {
+    "std_expr": False,
+    "contract_size": 10,
+    "entry_dte": Period.FOUR_WEEKS.value,
+    "exit_dte": None
+}
+
+
+def _create_legs(data, leg):
+    return (
+        data
+        .pipe(opt_type, option_type=leg[0])
+        .assign(ratio=leg[1])
+    )
+
+
+def _apply_filters(legs, filters):
+    if not filters:
+        return legs
+    else:
+        return [reduce(lambda l, f: getattr(fil, f)(l, filters[f], idx), filters, leg)
+                for idx, leg in enumerate(legs)]
+
+
+def _filter_data(data, filters):
+    data = data if isinstance(data, list) else [data]
+    return pd.concat(_apply_filters(data, filters))
+
+
+def create_spread(data, leg_structs, filters):
+    legs = [_create_legs(data, leg) for leg in leg_structs]
+
+    # merge and apply leg filters to create spread
+    filters = {**default_entry_filters, **filters}
+    entry_filters = {f: filters[f]
+                     for f in filters if (not f.startswith('entry_spread') and
+                                          not f.startswith('exit_'))}
+    spread = _filter_data(legs, entry_filters)
+
+    # apply spread level filters to spread
+    spread_filters = {f: filters[f]
+                      for f in filters if f.startswith('entry_spread')}
+    return _filter_data(spread, spread_filters)
+
+
 # this is the main function that runs the backtest engine
-def simulate():
-    pass
+def run(data, trades, filters, mode='midpoint'):
+    trades = trades if isinstance(trades, list) else [trades]
+
+    # merge trades from multiple underlying symbols if applicable
+    all_trades = pd.concat(trades).sort_values(sort_by)
+
+    # for each option to be traded, determine the historical price action
+    exit_filters = {f: filters[f] for f in filters if f.startswith('exit_')}
+    res = pd.merge(all_trades, data, on=on, suffixes=(
+        '_entry', '_exit')).pipe(_filter_data, exit_filters)
+
+    # calculate the p/l for the trades
+    if mode == 'midpoint':
+        res['entry_opt_price'] = res[['bid_entry', 'ask_entry']].mean(axis=1)
+        res['exit_opt_price'] = res[['bid_exit', 'ask_exit']].mean(axis=1)
+        res['entry_price'] = res['entry_opt_price'] * \
+            res['ratio'] * res['contracts']
+        res['exit_price'] = res['exit_opt_price'] * \
+            res['ratio'] * res['contracts']
+        res['profit'] = res['exit_price'] - res['entry_price']
+    elif mode == 'market':
+        pass
+
+    res.rename(columns={
+        'quote_date_entry': 'entry_date',
+        'quote_date_exit': 'exit_date',
+        'delta_entry': 'entry_delta',
+        'underlying_price_entry': 'entry_stk_price',
+        'underlying_price_exit': 'exit_stk_price',
+        'dte_entry': 'DTE'
+    }, inplace=True)
+
+    output_format = ['entry_date', 'exit_date', 'expiration', 'DTE', 'ratio', 'contracts',
+                     'option_type', 'strike', 'entry_delta', 'entry_stk_price',
+                     'exit_stk_price', 'entry_opt_price', 'exit_opt_price', 'entry_price',
+                     'exit_price', 'profit']
+
+    total_profit = res['profit'].sum().round(2)
+
+    return total_profit, res[output_format]
--- optopsy/option_strategies.py
+++ optopsy/option_strategies.py
@@ -1,62 +1,73 @@
-from .enums import OrderAction, OptionType
-from .option_spreads import create_spread
+from .enums import OptionType
+from .backtest import create_spread
 
 
-def long_call(data, filters):
+def _add_date_range(s, e, f):
+    f['start_date'] = s
+    f['end_date'] = e
+    return f
+
+
+def long_call(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
-        return OrderAction.BTO, create_spread(
+        return create_spread(
             data, [(OptionType.CALL, 1)], filters)
 
 
-def short_call(data, filters):
+def short_call(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
-        return OrderAction.STO, create_spread(
-            data, [(OptionType.CALL, 1)], filters)
+        return create_spread(data, [(OptionType.CALL, 1)], filters)
 
 
-def long_put(data, filters):
+def long_put(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
-        return OrderAction.BTO, create_spread(
-            data, [(OptionType.PUT, 1)], filters)
+        return create_spread(data, [(OptionType.PUT, 1)], filters)
 
 
-def short_put(data, filters):
+def short_put(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
-        return OrderAction.STO, create_spread(
-            data, [(OptionType.PUT, 1)], filters)
+        return create_spread(data, [(OptionType.PUT, 1)], filters)
 
 
-def long_call_spread(data, filters):
+def long_call_spread(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
         legs = [(OptionType.CALL, -1), (OptionType.CALL, 1)]
-        return OrderAction.BTO, create_spread(data, legs, filters)
+        return create_spread(data, legs, filters)
     else:
         raise ValueError(
             "Long delta must be less than short delta for long call spreads!")
 
 
-def short_call_spread(data, filters):
+def short_call_spread(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
         legs = [(OptionType.CALL, 1), (OptionType.CALL, -1)]
-        return OrderAction.STO, create_spread(data, legs, filters)
+        return create_spread(data, legs, filters)
     else:
         raise ValueError(
             "Short delta must be less than long delta for short call spreads!")
 
 
-def long_put_spread(data, filters):
+def long_put_spread(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
         legs = [(OptionType.PUT, 1), (OptionType.PUT, -1)]
-        return OrderAction.BTO, create_spread(data, legs, filters)
+        return create_spread(data, legs, filters)
     else:
         raise ValueError(
             "Short delta must be less than long delta for long put spreads!")
 
 
-def short_put_spread(data, filters):
+def short_put_spread(data, start_date, end_date, filters):
+    filters = _add_date_range(start_date, end_date, filters)
     if _filter_check(filters):
         legs = [(OptionType.PUT, -1), (OptionType.PUT, 1)]
-        return OrderAction.STO, create_spread(data, legs, filters)
+        return create_spread(data, legs, filters)
     else:
         raise ValueError(
             "Long delta must be less than short delta for short put spreads!")
