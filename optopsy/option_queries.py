4706cefeae03ff6efd372501c652061498c01616
michaelchu <mchchu88@gmail.com>
2018-11-14 01:24:28 -0500

    updated License and setup.py

--- optopsy/option_queries.py
+++ optopsy/option_queries.py
@@ -1,9 +1,19 @@
-"""
-The goal of this module is to abstract away dataframe manipulation
-functions and provide an easy to use interface to query for specific
-option legs in a dataframe. All functions will return a new pandas
-dataframe to allow for method chaining
-"""
+#     Optopsy - Python Backtesting library for options trading strategies
+#     Copyright (C) 2018  Michael Chu
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from optopsy.data import fields
 from optopsy.enums import Period, OptionType
 
--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 setup(name='optopsy',
       description='Python Backtesting library for options trading strategies',
       long_description=open("README.md").read(),
-      version='0.1.0',
+      version='1.0.0',
       url='https://github.com/michaelchu/optopsy',
       author='Michael Chu',
       author_email='mchchu88@gmail.com',
-      license='MIT',
+      license='GPL-3.0-or-later',
       classifiers=[
           "Operating System :: OS Independent",
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3'
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3.6'
       ],
       packages=['optopsy']
       )
