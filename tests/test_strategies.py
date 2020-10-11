1231f5d76ee7c92675c5924da3c3a8bcd21936b2
Michael Chu <mchchu88@gmail.com>
2020-10-11 01:26:14 -0400

    Redefine option entry and exit column names (#53)

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -45,22 +45,40 @@ def data():
     return pd.DataFrame(data=d, columns=cols)
 
 
-def test_single_calls_raw(data):
+def test_single_long_calls_raw(data):
     results = long_calls(data, raw=True)
     assert len(results) == 2
     assert list(results.columns) == single_strike_internal_cols
     assert "call" in list(results["option_type"].values)
-    assert round(results.iloc[0]["profit_pct"], 2) == 0.01
-    assert round(results.iloc[1]["profit_pct"], 2) == -0.17
+    assert round(results.iloc[0]["pct_change"], 2) == 0.01
+    assert round(results.iloc[1]["pct_change"], 2) == -0.17
 
 
-def test_single_puts_raw(data):
+def test_single_long_puts_raw(data):
     results = long_puts(data, raw=True)
     assert len(results) == 2
     assert list(results.columns) == single_strike_internal_cols
     assert "put" in list(results["option_type"].values)
-    assert round(results.iloc[0]["profit_pct"], 2) == -1
-    assert round(results.iloc[1]["profit_pct"], 2) == -1
+    assert round(results.iloc[0]["pct_change"], 2) == -1
+    assert round(results.iloc[1]["pct_change"], 2) == -1
+
+
+def test_single_short_calls_raw(data):
+    results = short_calls(data, raw=True)
+    assert len(results) == 2
+    assert list(results.columns) == single_strike_internal_cols
+    assert "call" in list(results["option_type"].values)
+    assert round(results.iloc[0]["pct_change"], 2) == 0.01
+    assert round(results.iloc[1]["pct_change"], 2) == -0.17
+
+
+def test_single_short_puts_raw(data):
+    results = short_puts(data, raw=True)
+    assert len(results) == 2
+    assert list(results.columns) == single_strike_internal_cols
+    assert "put" in list(results["option_type"].values)
+    assert round(results.iloc[0]["pct_change"], 2) == -1
+    assert round(results.iloc[1]["pct_change"], 2) == -1
 
 
 def test_singles_long_calls(data):
@@ -83,7 +101,7 @@ def test_singles_short_calls(data):
     results = short_calls(data)
     assert len(results) == 1
     assert results.iloc[0]["count"] == 2.0
-    assert round(results.iloc[0]["mean"], 2) == 0.08
+    assert round(results.iloc[0]["mean"], 2) == -0.08
     assert list(results.columns) == single_strike_external_cols + describe_cols
 
 
@@ -91,17 +109,26 @@ def test_singles_short_puts(data):
     results = short_puts(data)
     assert len(results) == 1
     assert results.iloc[0]["count"] == 2.0
-    assert round(results.iloc[0]["mean"], 2) == 1.0
+    assert round(results.iloc[0]["mean"], 2) == -1.0
     assert list(results.columns) == single_strike_external_cols + describe_cols
 
 
-def test_straddles_raw(data):
+def test_straddles_long_raw(data):
     results = long_straddles(data, raw=True)
     assert list(results.columns) == straddle_internal_cols
     assert results.iloc[0]["option_type_leg1"] == "put"
     assert results.iloc[0]["option_type_leg2"] == "call"
-    assert round(results.iloc[0]["profit_pct"], 2) == -0.43
-    assert round(results.iloc[1]["profit_pct"], 2) == -0.62
+    assert round(results.iloc[0]["pct_change"], 2) == -0.43
+    assert round(results.iloc[1]["pct_change"], 2) == -0.62
+
+
+def test_straddles_short_raw(data):
+    results = short_straddles(data, raw=True)
+    assert list(results.columns) == straddle_internal_cols
+    assert results.iloc[0]["option_type_leg1"] == "put"
+    assert results.iloc[0]["option_type_leg2"] == "call"
+    assert round(results.iloc[0]["pct_change"], 2) == 0.43
+    assert round(results.iloc[1]["pct_change"], 2) == 0.62
 
 
 def test_long_straddles(data):
@@ -120,13 +147,22 @@ def test_short_straddles(data):
     assert list(results.columns) == single_strike_external_cols + describe_cols
 
 
-def test_strangles_raw(data):
+def test_strangles_long_raw(data):
     results = long_strangles(data, raw=True)
     assert len(results) == 1
     assert list(results.columns) == double_strike_internal_cols
     assert results.iloc[0]["option_type_leg1"] == "put"
     assert results.iloc[0]["option_type_leg2"] == "call"
-    assert round(results.iloc[0]["profit_pct"], 2) == -0.57
+    assert round(results.iloc[0]["pct_change"], 2) == -0.57
+
+
+def test_strangles_short_raw(data):
+    results = short_strangles(data, raw=True)
+    assert len(results) == 1
+    assert list(results.columns) == double_strike_internal_cols
+    assert results.iloc[0]["option_type_leg1"] == "put"
+    assert results.iloc[0]["option_type_leg2"] == "call"
+    assert round(results.iloc[0]["pct_change"], 2) == 0.57
 
 
 def test_long_strangles(data):
@@ -143,3 +179,12 @@ def test_short_strangles(data):
     assert results.iloc[0]["count"] == 1.0
     assert round(results.iloc[0]["mean"], 2) == 0.57
     assert list(results.columns) == double_strike_external_cols + describe_cols
+
+
+def test_long_call_spread_raw(data):
+    results = long_call_spread(data, raw=True)
+    assert len(results) == 1
+    assert list(results.columns) == double_strike_internal_cols
+    assert results.iloc[0]["option_type_leg1"] == "call"
+    assert results.iloc[0]["option_type_leg2"] == "call"
+    assert round(results.iloc[0]["pct_change"], 2) == 0.81
